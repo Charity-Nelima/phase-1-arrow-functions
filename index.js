d1a99a022c20c6205592dafa61cf93b1c644c2f9
Charity Nelima <wa.nelimacharity@gmail.com>
2022-08-17 16:28:02 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+// function divide (){
+
+// }
+// let divide = function((a = 2000), (b = 100)){
+// console.log a%b
+// // }
+// const add = (parameter1, parameter2) => parameter1 + parameter2;
+// const divide = (num1, num2) => num1 / num2;
+// // return num1 / num2;
+// divide(2000,100);
+// const divide = function (num1, num2) {
+//     const divide = (num1, num2) => {
+//         return (2000 / 100);
+//     }
+// }   
+// const square= (parameter1, parameter2) => parameter1 * parameter2;
+// const nums = [1,2,3];
+// const squares = nums.map(x => x ** 2); 
+// squares (1,1);
+// console.log(1,1);
+function divide(a =2000,b=100) {
+    return a/b;
+}
+const square = (num) => {
+    return (num * num);
+}
+const add = (num3, num4) => {
+    return (num3 + num4);
+} 
